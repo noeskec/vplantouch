e6b7c69b5b956721b92788b40592d0289425989e
noeskec <noe@ghse.de>
2022-05-03 09:26:17 +0200

    ensure UTF-8 character set when writing to database in updatedb

--- vplan_updatedb/lib/db.php
+++ vplan_updatedb/lib/db.php
@@ -1,13 +1,14 @@
 <?php 
 
-
 $dbUser = json_decode(file_get_contents("../etc/logins.json"),true)["Database"]["Username"];
 $dbPassword = json_decode(file_get_contents("../etc/logins.json"),true)["Database"]["Password"];
 $dbHost = json_decode(file_get_contents("../etc/logins.json"),true)["Database"]["Host"];
 $dbDatabase = json_decode(file_get_contents("../etc/logins.json"),true)["Database"]["Database-Name"];
+
 $db=mysqli_connect($dbHost,$dbUser,$dbPassword,$dbDatabase);
+$db->set_charset("utf8mb4");
+
 $dbTMP=mysqli_connect($dbHost,$dbUser,$dbPassword);
-
-
+$dbTMP->set_charset("utf8mb4");
 
  ?>
